4e3d90fb9eeb19bdf55ae843b748ca8968659037
notrodash <n@notr.co>
2014-06-30 15:45:51 +0100

    Now an Android Studio project. Replaced all tabs with four spaces.
    Signed-off-by: notrodash <n@notr.co>

---
+++ app/src/main/java/com/odo/kcl/mobileminer/CkanUidGetter.java
@@ -0,0 +1,58 @@
+package com.odo.kcl.mobileminer;
+
+import java.util.concurrent.ExecutionException;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.odo.kcl.mobileminer.MinerTables.BookKeepingTable;
+import com.odo.kcl.mobileminer.MinerTables.MinerLogTable;
+
+public class CkanUidGetter {
+    Context context;
+
+    public CkanUidGetter(Context ctx) {
+        context = ctx;
+    }
+
+    public String getUid() {
+        return getUid(false);
+    }
+
+    public String getUid(boolean forceRefresh) {
+        String uid;
+
+        MinerData helper = new MinerData(context);
+        uid = null;
+
+        if (!forceRefresh) {
+            uid = helper.getBookKeepingKey(helper.getReadableDatabase(), "ckanuid");
+        }
+
+        if (uid == null) {
+            try {
+                uid = (String) new CkanUidRequest().execute(new Context[]{context}).get();
+            }
+            catch (InterruptedException e) {
+                uid = null;
+            }
+            catch (ExecutionException e) {
+                uid = null;
+            }
+
+            if (uid != null) {
+                helper.deleteBookKeepingKey(helper.getWritableDatabase(), "ckanuid");
+                helper.setBookKeepingKey(helper.getWritableDatabase(), "ckanuid", uid);
+            }
+
+        }
+
+        helper.close();
+        return uid;
+
+    }
+
+}
---
+++ app/src/main/java/com/odo/kcl/mobileminer/NotificationService.java
@@ -0,0 +1,77 @@
+// Licensed under the Apache License Version 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+package com.odo.kcl.mobileminer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ServiceInfo;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+
+public class NotificationService extends AccessibilityService {
+
+    @Override
+    public void onServiceConnected() {
+        // http://stackoverflow.com/questions/14540394/listen-to-incoming-whatsapp-messages-notifications
+        // http://developer.android.com/training/accessibility/service.html#create
+        // http://stackoverflow.com/questions/7937794/how-to-get-installed-applications-permissions
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        ArrayList<String> netEnabledPackageNames = new ArrayList<String>();
+
+        for (ApplicationInfo appInfo : packages) {
+            String[] permissions;
+            try {
+                permissions = pm.getPackageInfo(appInfo.packageName, PackageManager.GET_PERMISSIONS).requestedPermissions;
+
+                if (permissions != null) {
+                    for (String permission: permissions) {
+                        if (permission.equals("android.permission.INTERNET")) {
+                            netEnabledPackageNames.add(appInfo.packageName);
+                            Log.i("NotificationService",appInfo.packageName);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            catch (NameNotFoundException e) {
+
+            }
+
+        }
+
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+        info.packageNames = netEnabledPackageNames.toArray(new String[netEnabledPackageNames.size()]);
+        info.feedbackType = AccessibilityServiceInfo.DEFAULT;
+        info.notificationTimeout = 100;
+        this.setServiceInfo(info);
+
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        MinerData helper = new MinerData(this);
+        helper.putNotification(helper.getWritableDatabase(), (String) event.getPackageName(),new Date());
+        // None of our business...
+//        helper.putNotification(helper.getWritableDatabase(), (String) event.getPackageName(),
+//            TextUtils.join(" ", event.getText()),new Date());
+        helper.close();
+    }
+
+    @Override
+    public void onInterrupt() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
