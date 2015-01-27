104ab06f7e9989d507363035a1c0d1251dcd8139
Giles Richard Greenway <augeas@gmail.com>
2015-01-27 19:42:40 +0000

    Do away with somewhat misleading geo-ip maps...  On branch alt-master 	modified:   src/uk/ac/kcl/odo/mobileminer/activities/adapters/SocketAdapter.java

--- src/uk/ac/kcl/odo/mobileminer/activities/adapters/SocketAdapter.java
+++ src/uk/ac/kcl/odo/mobileminer/activities/adapters/SocketAdapter.java
@@ -68,18 +68,20 @@ public class SocketAdapter extends android.widget.BaseExpandableListAdapter {
 
 	        @Override
 	        public void onClick(View v) {
-	        	Log.i("MobileMiner",childText);
 	        	
-	        	String ip = childText.split("\\s+")[1].split(":")[0];
-	        	Intent mapIntent = new GeoIpGetter(context).getMapIntent(ip);
-            	if (mapIntent == null) {
-            		Toast.makeText(context, "Can't get geoIP data...", Toast.LENGTH_SHORT).show();
-            	}
-            	else {
-            		Intent maapIntent = new Intent(context, MapActivity.class);
-            		mapIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            		context.startActivity(mapIntent);
-            	}                
+	        	//Log.i("MobileMiner",childText);
+	        	
+	        	// Do away with this, the geoIps are somewhat misleading...
+	        	//String ip = childText.split("\\s+")[1].split(":")[0];
+	        	//Intent mapIntent = new GeoIpGetter(context).getMapIntent(ip);
+            	//if (mapIntent == null) {
+            	//	Toast.makeText(context, "Can't get geoIP data...", Toast.LENGTH_SHORT).show();
+            	//}
+            	//else {
+            	//	Intent mapIntent = new Intent(context, MapActivity.class);
+            	//	mapIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            	//	context.startActivity(mapIntent);
+            	//}                
 	        }
 	    });
 		
